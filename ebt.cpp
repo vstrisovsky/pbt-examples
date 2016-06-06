3e088d4ec34dc3b292895fbef7581c86cccab26e
Vladimir Strisovsky <vladimir.strisovsky@kistler.com>
2016-06-06 10:10:41 +0200

    fixed makefile issue

--- ebt.cpp
+++ ebt.cpp
@@ -1,9 +1,8 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 #include "quicksort.h"
 
-TEST_CASE( "quick sort", "..." )
+TEST_CASE( "quick sort", "[ebt]" )
 {
   SECTION("sort vector")
   {
