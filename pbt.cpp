c8c06b155d32b35ff751b09d4c483914ba2a8ea9
Vladimir Strisovsky <vladimir.strisovsky@kistler.com>
2016-06-30 10:44:47 +0200

    refactored pbt tests

--- pbt.cpp
+++ pbt.cpp
@@ -1,62 +1,45 @@
 #include "catch.hpp"
 #include "quicksort.h"
 #include "cppqc.h"
-
+#include "cppqc/CompactCheck.h"
 
 TEST_CASE( "property based testing: quick sort", "[pbt]" )
 {
-  struct QuickSortOrdered : cppqc::Property<std::vector<int>>
-  {
-      bool check(const std::vector<int> &v) const override
-      {
-          std::vector<int> sortedCollection(v);
-          quicksort(sortedCollection.begin(), sortedCollection.end());
-          return std::is_sorted(sortedCollection.begin(), sortedCollection.end());
-      }
-      std::string name() const override
-      {
-          return "sorting should be sorted";
-      }
-      bool trivial(const std::vector<int> &v) const override
-      {
-          return v.size() == 0 || v.size() == 1;
-      }
-      std::string classify(const std::vector<int> &v) const override
-      {
-          return std::to_string(v.size());
-      }
-  };
-
   SECTION("the output shoud be ordered")
   {
-      REQUIRE(cppqc::quickCheckOutput(QuickSortOrdered()).result == cppqc::QC_SUCCESS);
-  }
+      auto test = cppqc::gen<std::vector<int>>()
+          .property("sorting should be sorted",
+            [](const std::vector<int>& v)
+            {
+                std::vector<int> sortedCollection(v);
+                quicksort(sortedCollection.begin(), sortedCollection.end());
+                return std::is_sorted(sortedCollection.begin(), sortedCollection.end());
+            })
+          .trivial
+            ([](const std::vector<int>& v)
+            {
+              return v.size() == 0;
+            })
+         .classify
+            ([](const std::vector<int>& v)
+            {
+                return std::to_string(v.size());
+            });
 
-  struct QuickSortPermutation : cppqc::Property<std::vector<int>>
-  {
-      bool check(const std::vector<int> &v) const override
-      {
-          std::vector<int> sortedCollection(v);
-          quicksort(sortedCollection.begin(), sortedCollection.end());
-          return std::is_permutation(sortedCollection.begin(), sortedCollection.end(), v.begin());
-      }
-      std::string name() const override
-      {
-          return "input - output permutation";
-      }
-      bool trivial(const std::vector<int> &v) const override
-      {
-          return v.size() == 0 || v.size() == 1;
-      }
-      std::string classify(const std::vector<int> &v) const override
-      {
-          return std::to_string(v.size());
-      }
-  };
+      REQUIRE(test.test().result == cppqc::QC_SUCCESS);
+  }
 
   SECTION("the output shoud be permutation of the input")
   {
-      REQUIRE(cppqc::quickCheckOutput(QuickSortPermutation()).result == cppqc::QC_SUCCESS);
-  }
+      auto test = cppqc::gen<std::vector<int>>()
+          .property("input - output permutation",
+            [](const std::vector<int>& v)
+            {
+                std::vector<int> sortedCollection(v);
+                quicksort(sortedCollection.begin(), sortedCollection.end());
+                return std::is_permutation(sortedCollection.begin(), sortedCollection.end(), v.begin());
+            });
 
+      REQUIRE(test.test().result == cppqc::QC_SUCCESS);
+  }
 }
